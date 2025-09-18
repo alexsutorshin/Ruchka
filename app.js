f19790c9c46a13fca5609b589c2a05d1d0b9d25f
alexsutorshin <alexsutorshin@gmail.com>
2025-09-19 00:23:16 +0300

    Fix CORS credentials - enable credentials when origin is present

--- app.js
+++ app.js
@@ -32,15 +32,16 @@ app.use((req, res, next) => {
   const origin = req.headers.origin;
   if (origin) {
     res.header("Access-Control-Allow-Origin", origin);
+    res.header("Access-Control-Allow-Credentials", "true");
   } else {
     res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Credentials", "false");
   }
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.header(
     "Access-Control-Allow-Headers",
     "Content-Type, x-ingest-token, Authorization"
   );
-  res.header("Access-Control-Allow-Credentials", "false");
 
   if (req.method === "OPTIONS") {
     res.sendStatus(200);
