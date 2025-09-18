cfb5b67109be54e647fcc6feeedca2bc70079472
alexsutorshin <alexsutorshin@gmail.com>
2025-09-18 22:22:41 +0300

    Fix CORS headers formatting

--- app.js
+++ app.js
@@ -9,11 +9,14 @@ app.use(helmet());
 
 // CORS middleware
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, x-ingest-token, Authorization');
-  
-  if (req.method === 'OPTIONS') {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Content-Type, x-ingest-token, Authorization"
+  );
+
+  if (req.method === "OPTIONS") {
     res.sendStatus(200);
   } else {
     next();
