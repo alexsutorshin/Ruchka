434f5164014bf3cfbe2bbdc8d98880c18cef7247
alexsutorshin <alexsutorshin@gmail.com>
2025-09-18 22:08:29 +0300

    Add custom CORS middleware with explicit headers

--- app.js
+++ app.js
@@ -6,6 +6,20 @@ require("dotenv").config();
 
 const app = express();
 app.use(helmet());
+
+// CORS middleware
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, x-ingest-token, Authorization');
+  
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+});
+
 app.use(cors({ origin: true, credentials: true }));
 app.use(express.json({ limit: "10mb" }));
 
